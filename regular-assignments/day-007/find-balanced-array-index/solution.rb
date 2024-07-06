b5b113e52217cf9c300e6c6552676fe55fa1dab9
Prasad Surase <prasad.surase@coupa.com>
2024-07-06 20:19:11 +0530

    Added solution to find balanced array indices based on even and odd prefix sum arrays

---
+++ regular-assignments/day-007/find-balanced-array-index/solution.rb
@@ -0,0 +1,50 @@
+class Solution
+  def solve(arr)
+    prefix_even_sum_arr = create_even_prefix_sum_arr(arr)
+    prefix_odd_sum_arr = create_odd_prefix_sum_arr(arr)
+    i = 0
+    count = 0
+    while i < arr.length
+      if i.zero?
+        sum_odd = prefix_even_sum_arr[arr.length - 1] - prefix_even_sum_arr[i]
+        sum_even = prefix_odd_sum_arr[arr.length - 1] - prefix_odd_sum_arr[i]
+      else
+        sum_odd = prefix_odd_sum_arr[i - 1] + (prefix_even_sum_arr[arr.length-1] - prefix_even_sum_arr[i-1+1])
+        sum_even = prefix_even_sum_arr[i - 1] + (prefix_odd_sum_arr[arr.length-1] - prefix_odd_sum_arr[i-1+1])
+      end
+      count += 1 if sum_even == sum_odd
+      i += 1
+    end
+
+    count
+  end
+
+  def create_odd_prefix_sum_arr(arr)
+    i = 1
+    prefix_sum = [0]
+    while i < arr.length
+      prefix_sum[i] = !(i % 2).zero? ? prefix_sum[i - 1] + arr[i] : prefix_sum[i - 1]
+      i += 1
+    end
+    puts "Arr: #{arr}, odd prefix sum: #{prefix_sum}"
+    prefix_sum
+  end
+
+  def create_even_prefix_sum_arr(arr)
+    i = 1
+    prefix_sum = [arr[0]]
+    while i < arr.length
+      prefix_sum[i] = (i % 2).zero? ? prefix_sum[i - 1] + arr[i] : prefix_sum[i - 1]
+      i += 1
+    end
+    puts "Arr: #{arr}, even prefix sum: #{prefix_sum}"
+    prefix_sum
+  end
+end
+
+puts '-' * 100
+puts Solution.new.solve([2, 1, 6, 4])
+# puts '-' * 100
+# puts Solution.new.solve([1, 1, 1])
+# puts '-' * 100
+# puts Solution.new.solve([1, 2, 3, 4, 5, 6, 7, 8, 9])
