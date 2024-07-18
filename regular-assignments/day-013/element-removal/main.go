c3bfb735e97a3310fe3da97cb47181351f80ad88
Prasad Surase <prasad.surase@coupa.com>
2024-07-18 14:51:45 +0530

    Added solution to get the minimum cost of the array

---
+++ regular-assignments/day-013/element-removal/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"sort"
+)
+
+func main() {
+	fmt.Printf("%v\n", solve([]int{2, 1}))
+	fmt.Printf("%v\n", solve([]int{5}))
+	fmt.Printf("%v\n", solve([]int{8, 0, 10}))
+}
+
+func solve(arr []int) int {
+	sort.Ints(arr)
+	slices.Reverse(arr)
+
+	// cost = 0
+	//   i = 0
+	//   while i < arr.length
+	//     cost += arr[i..-1].sum
+	//     i += 1
+	//   end
+	//   cost
+
+	cost := 0
+	for i := 0; i < len(arr); i++ {
+		for j := i; j < len(arr); j++ {
+			cost += arr[j]
+		}
+	}
+	return cost
+}
